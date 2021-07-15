76b2151656a58780d23790cd9cf341bfd72dd755
Nikolay <ni@portalone.com>
2021-07-15 23:43:34 +0400

    feat: external tileset demo

---
+++ demo/md-external.js
@@ -0,0 +1,22 @@
+//inject TILED importer but skip middleware Injecting, only mixins and configs
+PIXI.TiledOG.Inject(PIXI, { injectMiddleware: true, debugContainers: true });
+
+var app = new PIXI.Application({
+	width: 512,
+	height: 512,
+});
+
+document.body.appendChild(app.view);
+
+//load map with dependencies
+app.loader.add('map', './maps/map-with-external-tileset.json').load(loaded);
+
+function loaded() {
+	const create = app.loader.resources['map'].stage;
+
+	app.stage.addChild(create);
+
+	app.ticker.add(gameLoop);
+}
+
+function gameLoop(dt) {}
